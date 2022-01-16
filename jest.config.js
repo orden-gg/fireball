0fd7995b5b4909b71949758756dfb137c6d65d6e
Dmytro Kot <dmutrokot94@gmail.com>
2022-01-16 12:59:08 +0200

    Citadel Map: Add transformIgnorePatterns option to jest config for @ion-phaser/react

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,10 @@ const config = {
     moduleNameMapper: {
         "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/src/__mocks__/fileMock.js",
         "\\.(css|less)$": "<rootDir>/src/__mocks__/fileMock.js"
-    }
+    },
+    transformIgnorePatterns: [
+        "node_modules/(?!@ion-phaser/react)"
+    ]
 };
   
 module.exports = config;
