b99391ce8ab4c5555deb8aa0c9d6b36a5a47dd9e
yanik <yanikkoval@gmail.com>
2022-03-17 22:50:28 +0700

    security warning + better text

--- src/pages/Autopet/components/AutopetHeader.js
+++ src/pages/Autopet/components/AutopetHeader.js
@@ -23,9 +23,20 @@ export default function AutopetHeader() {
             <h1 className={classes.autopetTitle}>Trustless Autopet</h1>
 
             <Typography className={classes.panelText}>
+                <h3>How it works?!</h3> <br/>
                 Minimalistic proxy-contract allowing you to pet your gotchis in trustless way. <br/>
                 Contract adress 0x715FB0175ebCa2802855D8AdCc93fd913EF60E93 and the code is <a href=''>verified</a> <br/>
-                This contract is fully open sourced, meaning any guild can setup it for themself! and verified code.  
+                This contract is fully open sourced, meaning any guild can setup it for themself!
+            </Typography>
+
+            <Typography className={classes.panelText}>
+                <h3>ASSETS SECURITY WARNING!</h3> <br/>
+                <b>make sure to verify URL and contract address before interacting with any similar interface</b> <br/>
+            </Typography>
+
+            <Typography className={classes.panelText}>
+                <h3>Which autopet to use?! </h3><br/>
+                <b>make sure to verify URL and contract address before interacting with any similar interface</b> <br/>
             </Typography>
 
             <Typography className={classes.autopetComplete}>
