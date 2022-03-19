cc9947d98d7b10d81e0cd4ea02e742957e0eb15e
yanik <yanikkoval@gmail.com>
2022-03-19 16:31:12 +0700

    autopet text

--- src/pages/Autopet/components/AutopetHeader.js
+++ src/pages/Autopet/components/AutopetHeader.js
@@ -13,21 +13,22 @@ export default function AutopetHeader() {
                 <div className={classes.autopetTermsBox}>
                     <h3 className={classes.autopetTermsTitle}>How it works?!</h3>
                     <Typography className={classes.autopetTermsText}>
-                        Minimalistic proxy-contract allowing you to pet your gotchis in trustless way.
+                        this is iterface for a minimalistic  proxy-(contract) allowing you to pet your gotchis in trustless way with a bot runned by ordenGG guild
                     </Typography>
                     <Typography className={classes.autopetTermsText}>
-                        Contract adress 0x715FB0175ebCa2802855D8AdCc93fd913EF60E93 and the code is 
-                        <span className={classes.autopetTermsTextHighlight}> verified</span>
-                    </Typography>
-                    <Typography className={classes.autopetTermsText}>
-                        This contract is fully open sourced, meaning any guild can setup it for themself!
+                        the (contract) and (ux) you browsing now is fully open sourced (MIT) meaning any guild can setup it for themself! the (bot) script for trustless transactions and petting timing will be released soon after we will help a few guilds to setup it for themself and produce the necceserry documentation
                     </Typography>
                 </div>
 
                 <div className={classes.autopetTermsBox}>
                     <h3 className={classes.autopetTermsTitle}>Which autopet to use?! </h3>
                     <Typography className={classes.autopetTermsText}>
-                        In gotchiverse petting delegation can be used for diplomatic gameplay. Can be or nutural service (petting fees are equal to roughly 50 (?) GHST locked) to form up a guild, or form of support or endorcment.
+                        In gotchiverse, petting delegation can be used as a form of <span className={classes.autopetTermsTextHighlight}>diplomatic gameplay</span> and coordination mechanics. By petting with guild you paying minor membership fee and claiming your aavegotchi loyalty
+                    </Typography>
+                    {/* gm, add change credentials to your guild */}
+                    <Typography className={classes.autopetTermsText}>
+                        <span className={classes.autopetTermsTextHighlight}>ordenGG</span> autopet adress is <span className={classes.autopetTermsTextHighlight}>0x715FB0175ebCa2802855D8AdCc93fd913EF60E93</span> and the code is 
+                        <span className={classes.autopetTermsTextHighlight}> open and verified o exlorer</span>
                     </Typography>
                 </div>
             </div>
