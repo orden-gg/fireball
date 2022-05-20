e161d4331c0c77cd6205311fd0d3a79d29266113
Dmytro Kot <dmutrokot94@gmail.com>
2022-05-20 15:47:23 +0300

    feat: Add GLTR balance in header

--- src/api/alchemica.api.js
+++ src/api/alchemica.api.js
@@ -2,13 +2,14 @@ import { ethers } from 'ethers';
 
 import ethersApi from './ethers.api';
 
-import { AKEK_ABI, ALPHA_ABI, FOMO_ABI, FUD_ABI } from 'data/abi/alchemica.abi';
-import { KEK_CONTRACT, ALPHA_CONTRACT, FOMO_CONTRACT, FUD_CONTRACT } from './common/constants';
+import { KEK_ABI, ALPHA_ABI, FOMO_ABI, FUD_ABI, GLTR_ABI } from 'data/abi/alchemica.abi';
+import { KEK_CONTRACT, ALPHA_CONTRACT, FOMO_CONTRACT, FUD_CONTRACT, GLTR_CONTRACT } from './common/constants';
 
-const akekContract = ethersApi.makeContract(KEK_CONTRACT, AKEK_ABI, 'polygon');
+const akekContract = ethersApi.makeContract(KEK_CONTRACT, KEK_ABI, 'polygon');
 const alphaContract = ethersApi.makeContract(ALPHA_CONTRACT, ALPHA_ABI, 'polygon');
 const fomoContract = ethersApi.makeContract(FOMO_CONTRACT, FOMO_ABI, 'polygon');
 const fudContract = ethersApi.makeContract(FUD_CONTRACT, FUD_ABI, 'polygon');
+const gltrContract = ethersApi.makeContract(GLTR_CONTRACT, GLTR_ABI, 'polygon');
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
@@ -27,4 +28,8 @@ export default {
     getFudBalance(address) {
         return fudContract.balanceOf(address).then(response => Number(ethers.utils.formatUnits(response._hex)));
     },
+
+    getGltrBalance(address) {
+        return gltrContract.balanceOf(address).then(response => Number(ethers.utils.formatUnits(response._hex)));
+    }
 }
