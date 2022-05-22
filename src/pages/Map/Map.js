0876698bac82cbc5e970ec15bda6facae9b1702c
Vladyslav <v2rockua@gmail.com>
2022-05-22 21:24:28 +0200

    multiselect bag fix

--- src/pages/Map/Map.js
+++ src/pages/Map/Map.js
@@ -19,6 +19,8 @@ export default function Map() {
     const [isListedLoaded, setIsListedLoaded] = useState(false);
     const [ownerRealm, setOwnerRealm] = useState({});
     const [isOwnerLoaded, setIsOwnerLoaded] = useState(false);
+    const [realmGroups, setRealmGroups] = useState([]);
+    const [groupsLoaded, setGroupsLoaded] = useState(false);
 
     const combineParcels = (listedParcels) => {
         return listedParcels.map(parcel => {
@@ -41,16 +43,12 @@ export default function Map() {
         let mounted = true;
 
         Promise.all([
-            thegraphApi.getParcelPriceByDirection({ size: 0, direction: "desc", limit: 500 }),
             thegraphApi.getParcelPriceByDirection({ size: 0, direction: "asc" }),
-            thegraphApi.getParcelPriceByDirection({ size: 1, direction: "desc", limit: 1000 }),
             thegraphApi.getParcelPriceByDirection({ size: 1, direction: "asc" }),
-            thegraphApi.getParcelPriceByDirection({ size: 2, direction: "desc", limit: 3000 }),
             thegraphApi.getParcelPriceByDirection({ size: 2, direction: "asc" }),
-            thegraphApi.getParcelPriceByDirection({ size: 3, direction: "desc", limit: 3000 }),
             thegraphApi.getParcelPriceByDirection({ size: 3, direction: "asc" }),
             thegraphApi.getAllListedParcels()
-        ]).then(([humbleAsc, humbleDesc, reasonableAsc, reasonableDesc, vSpaciousAsc, vSpaciousDesc, hSpaciousAsc, hSpaciousDesc, listedParcels]) => {
+        ]).then(([humbleAsc, reasonableAsc, vSpaciousAsc, hSpaciousAsc, listedParcels]) => {
             if (mounted) {
                 const combined = combineParcels(listedParcels);
 
@@ -61,9 +59,9 @@ export default function Map() {
                     icons: [<BlurOffIcon />, <BlurOnIcon />],
                     tooltip: 'Listed realm',
                     range: {
-                        humble: {min: humbleDesc, max: humbleAsc},
-                        reasonable: {min: reasonableDesc, max: reasonableAsc},
-                        spacious: {min: Math.min(hSpaciousDesc, vSpaciousDesc), max: Math.max(hSpaciousAsc, vSpaciousAsc)},
+                        humble: {min: humbleAsc, max: 500},
+                        reasonable: {min: reasonableAsc, max: 1000},
+                        spacious: {min: Math.min(vSpaciousAsc, hSpaciousAsc), max: 5000},
                     }
                 });
             }
@@ -81,6 +79,8 @@ export default function Map() {
 
     useEffect(() => {
         let mounted = true;
+
+        setIsOwnerLoaded(false);
 
         if (activeAddress) {
             thegraphApi.getRealmByAddress(activeAddress).then(ownerRealm => {
@@ -109,12 +109,20 @@ export default function Map() {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [activeAddress]);
 
+    useEffect(() => {
+        if (isOwnerLoaded && isListedLoaded) {
+            setRealmGroups([listedRealm, ownerRealm]);
+            setGroupsLoaded(true);
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [isOwnerLoaded, isListedLoaded]);
+
     return (
         <div className={classes.mapWrapper}>
             <Citadel
                 className={classes.citadel}
-                realmGroups={[listedRealm, ownerRealm]}
-                isLoaded={isOwnerLoaded && isListedLoaded}
+                realmGroups={realmGroups}
+                isLoaded={groupsLoaded}
             />
         </div>
     )
