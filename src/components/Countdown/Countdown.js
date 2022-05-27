93c16b6a642e848134f28734ded4cc009e5ef0c9
Dmytro Kot <dmutrokot94@gmail.com>
2022-05-27 18:59:31 +0300

    feat: Handle properly short, long and default long formats of time in Countdown

--- src/components/Countdown/Countdown.js
+++ src/components/Countdown/Countdown.js
@@ -1,66 +1,136 @@
 import React, { useState } from 'react';
+
 import { DateTime, Duration } from 'luxon';
 
 import useInterval from 'hooks/useInterval';
 
 const interval = 1000;
-const longFormat = {
-    y: { key: 'year', values: ['year', 'years'] },
-    MM: { key: 'month', values: ['month', 'months'] },
-    dd: { key: 'day', values: ['day', 'days'] },
-    hh: { key: 'hour', values: ['hour', 'hours'] },
-    mm: { key: 'minute', values: ['minute', 'minutes'] },
-    ss: { key: 'second', values: ['second', 'seconds'] }
+const defaultLongFormat = {
+    years: { key: 'y', values: ['year', 'years'], showIfZero: false },
+    months: { key: 'MM', values: ['month', 'months'], showIfZero: false },
+    days: { key: 'dd', values: ['day', 'days'], showIfZero: false },
+    hours: { key: 'hh', values: ['hour', 'hours'], showIfZero: false },
+    minutes: { key: 'mm', values: ['minute', 'minutes'], showIfZero: false },
+    seconds: { key: 'ss', values: ['second', 'seconds'], showIfZero: false }
 };
 
-export default function Countdown({ date, format, onEnd, replacementComponent }) {
-    const isInThePast = DateTime.fromISO(new Date(date).toISOString()).toMillis() < DateTime.now().toMillis();
+/***
+ * @param targetDate - luxon date in milliseconds
+ * @param shortFormat - 1y 2m 3d 4h
+ * @param longFormat - 1 year 2 months 3 days 4 hours
+ *
+ * If unit has to be shown if it's zero and previous units are also equal
+ * to zero - eg. 00 days 00 hours 21 minutes, set `@showIfZero` in formats configs to `true`
+ *
+ * @param onEnd - callback function that will trigger when current date is equal to `@targetDate`
+ * @param replacementComponent - component that will be placed instead of countdown if `@targetDate` is in the past
+*/
+export default function Countdown({ targetDate, shortFormat, longFormat, onEnd, replacementComponent }) {
+    const isInThePast = targetDate < DateTime.local().toMillis();
 
     const [isDateInThePast, setIsDateInThePast] = useState(isInThePast);
     const [countdown, setCountdown] = useState('');
 
     useInterval(() => {
-        const endDate = DateTime.fromISO(new Date(date).toISOString()).toMillis();
-        const now = DateTime.now().toMillis();
+        const now = DateTime.local().toMillis();
         let diff;
         let formattedTimeString;
 
-        if (endDate - now > 0) {
-            diff = endDate - now;
+        if (targetDate - now > 0) {
+            diff = targetDate - now;
 
             setIsDateInThePast(false);
         } else {
-            diff = now - endDate;
+            diff = now - targetDate;
 
             setIsDateInThePast(true);
         }
-        if (format) {
-            const mappedFormat = Object.entries(format).map(([key, value]) => `${key}'${value}'`).join(': ');
-            formattedTimeString = Duration.fromMillis(diff).toFormat(mappedFormat)
+        if (shortFormat) {
+            const formatKeys = Object.keys(shortFormat);
+            const units = Duration.fromMillis(diff).shiftTo(...formatKeys).toObject();
+            const mappedShortFormat = Object.keys(units)
+                .filter(key => shortFormat[key].showIfZero || getIsShowUnit(key, units))
+                .map(key => `${shortFormat[key].key}'${shortFormat[key].value}'`);
+
+            formattedTimeString = Duration.fromObject(units).toFormat(mappedShortFormat.join(' '));
+        } else if (longFormat) {
+            formattedTimeString = getLongFormattedTimeString(diff, longFormat);
         } else {
-            const mappedLongFormat = Object.entries(longFormat).map(([key, format]) => `${key}' ${format.values[0]}'`).join(' : ');
-            formattedTimeString = Duration.fromMillis(diff).toFormat(mappedLongFormat);
+            formattedTimeString = getLongFormattedTimeString(diff, defaultLongFormat);
         }
 
-        if (endDate - now > 0 ) {
+        if (targetDate - now > 0 ) {
             formattedTimeString = `In ${formattedTimeString}`;
-        } else if (endDate - now < 0) {
+        } else if (targetDate - now < 0) {
             formattedTimeString = `${formattedTimeString} ago`;
         }
 
         setCountdown(formattedTimeString);
 
-        if (parseInt(DateTime.fromMillis(endDate).toSeconds()) === parseInt(DateTime.fromMillis(now).toSeconds())) {
+        if (parseInt(DateTime.fromMillis(targetDate).toSeconds()) === parseInt(DateTime.fromMillis(now).toSeconds())) {
             if (Boolean(onEnd)) {
                 onEnd();
             }
         }
     }, interval);
 
+    const getLongFormattedTimeString = (diff, format) => {
+        const formatKeys = Object.keys(format);
+        const units = Duration.fromMillis(diff).shiftTo(...formatKeys).toObject();
+        const mappedLongFormat = Object.entries(units)
+            .filter(([key]) => format[key].showIfZero || getIsShowUnit(key, units))
+            .map(([key, unit]) => `${format[key].key} ${ parseInt(unit) !== 1 ?
+                `'${format[key].values[1]}'` : `'${format[key].values[0]}'`}`
+            );
+
+        return Duration.fromObject(units).toFormat(mappedLongFormat.join(' '));
+    }
+
+    const isShowUnitPredicate = (unitsKeys, units) => {
+        return unitsKeys.some(unitsKey => Boolean(units[unitsKey]) && units[unitsKey] > 0);
+    }
+
+    const getIsShowUnit = (key, units) => {
+        let unitsKeys = [];
+
+        switch (key) {
+            case 'years':
+                unitsKeys = ['years'];
+
+                break;
+            case 'months':
+                unitsKeys = ['years', 'months'];
+
+                break;
+            case 'days':
+                unitsKeys = ['years', 'months', 'days'];
+
+                break;
+            case 'hours':
+                unitsKeys = ['years', 'months', 'days', 'hours'];
+
+                break;
+            case 'minutes':
+                unitsKeys = ['years', 'months', 'days', 'hours', 'minutes'];
+
+                break;
+            case 'seconds':
+                unitsKeys = ['years', 'months', 'days', 'hours', 'minutes', 'seconds'];
+
+                break;
+            default:
+                unitsKeys.push('');
+
+                break;
+        }
+
+        return isShowUnitPredicate(unitsKeys, units)
+    }
+
     return (
         <>
             { !(isDateInThePast && replacementComponent) ? (
-                <div>Countdown: {countdown}</div>
+                <div>{countdown}</div>
             ): (
                 replacementComponent
             )}
--- src/pages/Client/routes/ClientTickets.js
+++ src/pages/Client/routes/ClientTickets.js
@@ -1,5 +1,7 @@
 import React, { useContext } from 'react';
 
+import { DateTime } from 'luxon';
+
 import ContentInner from 'components/Content/ContentInner';
 import Ticket from 'components/Items/Ticket/Ticket';
 import { ClientContext } from 'contexts/ClientContext';
@@ -8,17 +10,23 @@ import { routersStyles } from '../styles';
 import Countdown from 'components/Countdown/Countdown';
 
 export default function ClientTickets() {
-    const endDate = new Date(1653650851841);
+    const endDate = DateTime.fromJSDate(new Date(1784652751841)).toMillis();
     const classes = routersStyles();
     const { tickets, loadingTickets } = useContext(ClientContext);
 
     const shortFormat = {
-        y: 'y',
-        MM: 'm',
-        dd: 'd',
-        hh: 'h',
-        mm: 'm',
-        ss: 's'
+        years: { key: 'y', value: 'y', showIfZero: false },
+        months: { key: 'MM', value: 'm', showIfZero: false },
+        days: { key: 'dd', value: 'd', showIfZero: false },
+        hours: { key: 'hh', value: 'h', showIfZero: false },
+        minutes: { key: 'mm', value: 'm', showIfZero: false },
+        seconds: { key: 'ss', value: 's', showIfZero: false }
+    };
+    const longFormat = {
+        years: { key: 'y', values: ['year', 'years'], showIfZero: false },
+        months: { key: 'MM', values: ['month', 'months'], showIfZero: false },
+        days: { key: 'dd', values: ['day', 'days'], showIfZero: false },
+        hours: { key: 'hh', values: ['hour', 'hours'], showIfZero: false }
     };
 
     const onCountDownEnd = () => {
@@ -29,7 +37,7 @@ export default function ClientTickets() {
 
     return (
         <ContentInner dataLoading={loadingTickets} offset={208}>
-        <Countdown date={endDate} format={shortFormat} onEnd={onCountDownEnd} replacementComponent={replacementComponent} />
+        <Countdown targetDate={endDate} longFormat={longFormat} onEnd={onCountDownEnd} />
             <div className={classes.list}>
                 {
                     tickets.map((ticket, i)=>{
