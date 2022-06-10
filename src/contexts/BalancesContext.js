25608eb609046bb8a858ef57febaf95abf7f3932
dmutrokot <dmutrokot94@gmail.com>
2022-06-10 12:11:58 +0300

    feat: Create separate tokens prices context (#308)

--- src/contexts/BalancesContext.js
+++ src/contexts/BalancesContext.js
@@ -3,11 +3,12 @@ import { createContext, useContext, useEffect, useState } from 'react';
 import { AlphaTokenIcon, FomoTokenIcon, FudTokenIcon, GhstTokenIcon, GltrTokenIcon, KekTokenIcon } from 'components/Icons/Icons';
 import alchemicaApi from 'api/alchemica.api';
 import ghstApi from 'api/ghst.api';
-import quickSwapApi from 'api/quickswap.api';
 import { ALPHA_CONTRACT, DAI_CONTRACT, FOMO_CONTRACT, FUD_CONTRACT, GHST_CONTRACT, GLTR_CONTRACT, KEK_CONTRACT } from 'api/common/constants';
 import commonUtils from 'utils/commonUtils';
+import { TokenTypes } from 'data/types';
 
 import { LoginContext } from './LoginContext';
+import { TokensPricesContext } from './TokensPricesContext';
 
 export const BalancesContext = createContext({});
 
@@ -46,140 +47,132 @@ const BalancesContextProvider = (props) => {
     ];
 
     const { activeAddress } = useContext(LoginContext);
+    const { isPricesLoaded, tokensPrices } = useContext(TokensPricesContext);
 
+    const [isAmountsLoaded, setIsAmountsLoaded] = useState(false);
     const [isBalancesLoading, setIsBalancesLoading] = useState(false);
+    const [amounts, setAmounts] = useState({});
     const [tokens, setTokens] = useState([...initialTokensValues.map(token => ({
         amount: token.amount,
         balance: token.balance,
         imgSrc: token.imgSrc
     }))]);
 
-    const fetchInterval = 120; // seconds
+    const fetchInterval = 60; // seconds
 
     useEffect(() => {
-        let getBalances;
+        let mounted = true;
+        let getAmounts;
+        let interval;
+
         if (activeAddress) {
-            let mounted = true;
+            getAmounts = async function () {
+                setIsAmountsLoaded(false);
 
-            setIsBalancesLoading(true);
-
-            getBalances = async function () {
-                const [ghst, ghstPrice] = await getGhstAndPriceToToken(GHST_CONTRACT, DAI_CONTRACT);
-                const [fudAmount, fomoAmount, alphaAmount, kekAmount, gltrAmount, gshtAmount] = await Promise.all([
-                    alchemicaApi.getFudBalance(activeAddress),
-                    alchemicaApi.getFomoBalance(activeAddress),
-                    alchemicaApi.getAlphaBalance(activeAddress),
-                    alchemicaApi.getKekBalance(activeAddress),
-                    alchemicaApi.getGltrBalance(activeAddress),
-                    ghstApi.getBalanceOf(activeAddress)
-                ]);
-                const [fudToken, fomoToken, alphaToken, kekToken, gltrToken] = await Promise.all([
-                    quickSwapApi.getTokenData(FUD_CONTRACT),
-                    quickSwapApi.getTokenData(FOMO_CONTRACT),
-                    quickSwapApi.getTokenData(ALPHA_CONTRACT),
-                    quickSwapApi.getTokenData(KEK_CONTRACT),
-                    quickSwapApi.getTokenData(GLTR_CONTRACT)
-                ]);
-                const [fudPrice, fomoPrice, alphaPrice, kekPrice, gltrPrice] = await Promise.all([
-                    getTokenPrice(ghst, ghstPrice, fudToken),
-                    getTokenPrice(ghst, ghstPrice, fomoToken),
-                    getTokenPrice(ghst, ghstPrice, alphaToken),
-                    getTokenPrice(ghst, ghstPrice, kekToken),
-                    getTokenPrice(ghst, ghstPrice, gltrToken)
-                ]);
-                const ghstBalance = gshtAmount * ghstPrice;
-
-                const tokens = [
-                    {
-                        key: 'fud',
-                        icon: <FudTokenIcon height={14} width={14} />,
-                        amount: commonUtils.convertFloatNumberToSuffixNumber(fudAmount),
-                        pricePerToken: fudPrice.toFixed(3),
-                        balance: commonUtils.convertFloatNumberToSuffixNumber(fudPrice * fudAmount),
-                        swapUrl: generateSwapUrl(FUD_CONTRACT, GHST_CONTRACT)
-                    },
-                    {
-                        key: 'fomo',
-                        icon: <FomoTokenIcon height={14} width={14} />,
-                        amount: commonUtils.convertFloatNumberToSuffixNumber(fomoAmount),
-                        pricePerToken: fomoPrice.toFixed(3),
-                        balance: commonUtils.convertFloatNumberToSuffixNumber(fomoPrice * fomoAmount),
-                        swapUrl: generateSwapUrl(FOMO_CONTRACT, GHST_CONTRACT)
-                    },
-                    {
-                        key: 'alpha',
-                        icon: <AlphaTokenIcon height={14} width={14} />,
-                        amount: commonUtils.convertFloatNumberToSuffixNumber(alphaAmount),
-                        pricePerToken: alphaPrice.toFixed(3),
-                        balance: commonUtils.convertFloatNumberToSuffixNumber(alphaPrice * alphaAmount),
-                        swapUrl: generateSwapUrl(ALPHA_CONTRACT, GHST_CONTRACT)
-                    },
-                    {
-                        key: 'kek',
-                        icon: <KekTokenIcon height={14} width={14} />,
-                        amount: commonUtils.convertFloatNumberToSuffixNumber(kekAmount),
-                        pricePerToken: kekPrice.toFixed(2),
-                        balance: commonUtils.convertFloatNumberToSuffixNumber(kekPrice * kekAmount),
-                        swapUrl: generateSwapUrl(KEK_CONTRACT, GHST_CONTRACT)
-                    },
-                    {
-                        key: 'gltr',
-                        icon: <GltrTokenIcon height={14} width={14} />,
-                        amount: commonUtils.convertFloatNumberToSuffixNumber(gltrAmount),
-                        pricePerToken: gltrPrice.toFixed(5),
-                        balance: commonUtils.convertFloatNumberToSuffixNumber(gltrPrice * gltrAmount),
-                        swapUrl: generateSwapUrl(GLTR_CONTRACT, GHST_CONTRACT)
-                    },
-                    {
-                        key: 'ghst',
-                        icon: <GhstTokenIcon height={14} width={14} />,
-                        amount: commonUtils.convertFloatNumberToSuffixNumber(gshtAmount),
-                        pricePerToken: ghstPrice.toFixed(2),
-                        balance: commonUtils.convertFloatNumberToSuffixNumber(ghstBalance),
-                        swapUrl: generateSwapUrl(GHST_CONTRACT, DAI_CONTRACT)
-                    }
-                ];
+                const [fudAmount, fomoAmount, alphaAmount, kekAmount, gltrAmount, gshtAmount] = await getTokensAmounts(activeAddress);
 
                 if (mounted) {
-                    setTokens(tokens);
-                    setIsBalancesLoading(false);
+                    setAmounts({
+                        [TokenTypes.Fud]: fudAmount,
+                        [TokenTypes.Fomo]: fomoAmount,
+                        [TokenTypes.Alpha]: alphaAmount,
+                        [TokenTypes.Kek]: kekAmount,
+                        [TokenTypes.Gltr]: gltrAmount,
+                        [TokenTypes.Ghst]: gshtAmount
+                    });
+                    setIsAmountsLoaded(true);
                 }
             };
 
-            getBalances();
+            getAmounts();
 
-            const interval = setInterval(() => {
-                getBalances();
+            interval = setInterval(() => {
+                getAmounts();
             }, fetchInterval * 1000);
-
-            return () => {
-                mounted = false;
-
-                clearInterval(interval);
-            };
         }
+
+        return () => {
+            mounted = false;
+
+            clearInterval(interval);
+        };
     }, [activeAddress]);
 
-    const getGhstAndPriceToToken = async (ghstContract, tokenContract) => {
-        const [ghst, token] = await Promise.all([
-            quickSwapApi.getTokenData(ghstContract),
-            quickSwapApi.getTokenData(tokenContract)
+    useEffect(() => {
+        let mounted = true;
+
+        setIsBalancesLoading(!(isAmountsLoaded && isPricesLoaded));
+
+        if (isAmountsLoaded && isPricesLoaded) {
+            const tokens = [
+                {
+                    key: TokenTypes.Fud,
+                    icon: <FudTokenIcon height={14} width={14} />,
+                    amount: commonUtils.convertFloatNumberToSuffixNumber(amounts[TokenTypes.Fud]),
+                    pricePerToken: tokensPrices[TokenTypes.Fud].toFixed(3),
+                    balance: commonUtils.convertFloatNumberToSuffixNumber(tokensPrices[TokenTypes.Fud] * amounts[TokenTypes.Fud]),
+                    swapUrl: generateSwapUrl(FUD_CONTRACT, GHST_CONTRACT)
+                },
+                {
+                    key: TokenTypes.Fomo,
+                    icon: <FomoTokenIcon height={14} width={14} />,
+                    amount: commonUtils.convertFloatNumberToSuffixNumber(amounts[TokenTypes.Fomo]),
+                    pricePerToken: tokensPrices[TokenTypes.Fomo].toFixed(3),
+                    balance: commonUtils.convertFloatNumberToSuffixNumber(tokensPrices[TokenTypes.Fomo] * amounts[TokenTypes.Fomo]),
+                    swapUrl: generateSwapUrl(FOMO_CONTRACT, GHST_CONTRACT)
+                },
+                {
+                    key: TokenTypes.Alpha,
+                    icon: <AlphaTokenIcon height={14} width={14} />,
+                    amount: commonUtils.convertFloatNumberToSuffixNumber(amounts[TokenTypes.Alpha]),
+                    pricePerToken: tokensPrices[TokenTypes.Alpha].toFixed(3),
+                    balance: commonUtils.convertFloatNumberToSuffixNumber(tokensPrices[TokenTypes.Alpha] * amounts[TokenTypes.Alpha]),
+                    swapUrl: generateSwapUrl(ALPHA_CONTRACT, GHST_CONTRACT)
+                },
+                {
+                    key: TokenTypes.Kek,
+                    icon: <KekTokenIcon height={14} width={14} />,
+                    amount: commonUtils.convertFloatNumberToSuffixNumber(amounts[TokenTypes.Kek]),
+                    pricePerToken: tokensPrices[TokenTypes.Kek].toFixed(2),
+                    balance: commonUtils.convertFloatNumberToSuffixNumber(tokensPrices[TokenTypes.Kek] * amounts[TokenTypes.Kek]),
+                    swapUrl: generateSwapUrl(KEK_CONTRACT, GHST_CONTRACT)
+                },
+                {
+                    key: TokenTypes.Gltr,
+                    icon: <GltrTokenIcon height={14} width={14} />,
+                    amount: commonUtils.convertFloatNumberToSuffixNumber(amounts[TokenTypes.Gltr]),
+                    pricePerToken: tokensPrices[TokenTypes.Gltr].toFixed(5),
+                    balance: commonUtils.convertFloatNumberToSuffixNumber(tokensPrices[TokenTypes.Gltr] * amounts[TokenTypes.Gltr]),
+                    swapUrl: generateSwapUrl(GLTR_CONTRACT, GHST_CONTRACT)
+                },
+                {
+                    key: TokenTypes.Ghst,
+                    icon: <GhstTokenIcon height={14} width={14} />,
+                    amount: commonUtils.convertFloatNumberToSuffixNumber(amounts[TokenTypes.Ghst]),
+                    pricePerToken: tokensPrices[TokenTypes.Ghst].toFixed(2),
+                    balance: commonUtils.convertFloatNumberToSuffixNumber(tokensPrices[TokenTypes.Ghst] * amounts[TokenTypes.Ghst]),
+                    swapUrl: generateSwapUrl(GHST_CONTRACT, DAI_CONTRACT)
+                }
+            ];
+
+            if (mounted) {
+                setTokens(tokens);
+                setIsBalancesLoading(false);
+            }
+        }
+
+        return () => mounted = false;
+    }, [isAmountsLoaded, isPricesLoaded]);
+
+    const getTokensAmounts = (address) => {
+        return Promise.all([
+            alchemicaApi.getFudBalance(address),
+            alchemicaApi.getFomoBalance(address),
+            alchemicaApi.getAlphaBalance(address),
+            alchemicaApi.getKekBalance(address),
+            alchemicaApi.getGltrBalance(address),
+            ghstApi.getBalanceOf(address)
         ]);
-
-        const ghstTokenPair = await quickSwapApi.getPairData(ghst, token);
-        const ghstTokenRoute = quickSwapApi.getTokenRouteByPair(ghst, ghstTokenPair);
-        const ghstPriceToToken = Number(ghstTokenRoute.midPrice.toSignificant(6));
-
-        return [ghst, ghstPriceToToken];
-    };
-
-    const getTokenPrice = async (ghst, ghstPrice, token) => {
-        const ghstTokenPair = await quickSwapApi.getPairData(ghst, token);
-        const ghstTokenRoute = quickSwapApi.getTokenRouteByPair(token, ghstTokenPair);
-        const tokenToGhstPrice = Number(ghstTokenRoute.midPrice.toSignificant(6));
-        const tokenPrice = ghstPrice * tokenToGhstPrice;
-
-        return tokenPrice;
     };
 
     const generateSwapUrl = (inputToken, outputToken) => {
