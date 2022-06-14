ffef91c700f3f48d46b7698084f3e7f7ee47b705
Vladyslav <v2rockua@gmail.com>
2022-06-14 14:59:08 +0200

    feat: fix warning

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -5,10 +5,8 @@ import About from './components/About';
 
 import Portals from './components/Portals/Portals';
 import Team from './components/Team/Team';
-import styles from './styles';
 
 export default function Main() {
-    const classes = styles();
 
     return (
         <Grid container>
