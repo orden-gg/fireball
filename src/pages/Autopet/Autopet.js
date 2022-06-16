33d007afd09284ff45e6749f74eedc61f4f676ef
Dmytro Kot <dmutrokot94@gmail.com>
2022-06-16 14:33:41 +0300

    tech: Migrate AutopetInfo to TS

--- src/pages/Autopet/Autopet.js
+++ src/pages/Autopet/Autopet.js
@@ -1,7 +1,7 @@
 import AutopetContextProvider from './AutopetContextProvider';
 
 import AutopetHeader from './components/AutopetHeader';
-import AutopetInfo from './components/AutopetInfo/AutopetInfo';
+import { AutopetInfo } from './components/AutopetInfo/AutopetInfo';
 import AutopetSteps from './components/AutopetSteps/AutopetSteps';
 import styles from './styles';
 
--- src/pages/Autopet/components/AutopetInfo/AutopetInfo.tsx
+++ src/pages/Autopet/components/AutopetInfo/AutopetInfo.tsx
@@ -5,43 +5,45 @@ import CallMadeIcon from '@mui/icons-material/CallMade';
 import classNames from 'classnames';
 
 import { GhstTokenGif } from 'components/Icons/Icons';
+import { AUTOPET_CONTRACT } from 'api/common/constants';
 import autopetApi from 'api/autopet.api';
 import thegraph from 'api/thegraph.api';
+import commonUtils from 'utils/commonUtils';
 
 import AutopetInfoCard from './AutopetInfoCard';
 import { infoStyles } from '../../styles';
 
-import { AUTOPET_CONTRACT } from '../../../../api/common/constants';
-import commonUtils from '../../../../utils/commonUtils';
 
-export default function AutopetInfo() {
+export function AutopetInfo() {
     const classes = infoStyles();
-    const [fee, setFee] = useState(null);
-    const [frens, setFrens] = useState(null);
-    const [totalStaked, setTotalStaked] = useState(null);
-    const [totalGotchis, setTotalGotchis] = useState(null);
-    const [totalUsers, setTotalUsers] = useState(null);
+
+    const [fee, setFee] = useState<string>('');
+    const [frens, setFrens] = useState<string>('');
+    const [totalStaked, setTotalStaked] = useState<string>('');
+    const [totalGotchis, setTotalGotchis] = useState<string>('');
+    const [totalUsers, setTotalUsers] = useState<string>('');
 
     useEffect(() => {
-        autopetApi.getUsers().then(users => {
-            thegraph.getGotchisByAddresses(users).then( gotchis => {
+        autopetApi.getUsers().then((users: any) => {
+            thegraph.getGotchisByAddresses(users).then((gotchis: any) => {
                 setTotalGotchis(commonUtils.formatPrice(gotchis.length));
-                setTotalUsers(commonUtils.formatPrice(users.length));
             });
+
+            setTotalUsers(commonUtils.formatPrice(users.length));
         });
 
-        autopetApi.getFee().then(fee => {
+        autopetApi.getFee().then((fee: number) => {
             setFee(commonUtils.formatPrice(fee));
         });
 
-        autopetApi.getFrens().then(frens => {
+        autopetApi.getFrens().then((frens: number) => {
             setFrens(commonUtils.formatPrice(frens));
         });
     }, []);
 
     useEffect(() => {
-        if (totalUsers !== null & fee !== null) {
-            setTotalStaked(commonUtils.formatPrice(totalUsers*fee));
+        if (totalUsers !== '' && fee !== '') {
+            setTotalStaked(commonUtils.formatPrice(Number(totalUsers) * Number(fee)));
         }
     }, [totalUsers, fee]);
 
