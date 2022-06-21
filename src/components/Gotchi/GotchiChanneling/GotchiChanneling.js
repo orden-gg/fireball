f196f60126582c1cb64851ca18983fd53046b37b
dudendy <a.dude.koval@gmail.com>
2022-06-21 22:06:05 +0700

    feat: correct gotchi channeling icon display

--- src/components/Gotchi/GotchiChanneling/GotchiChanneling.js
+++ src/components/Gotchi/GotchiChanneling/GotchiChanneling.js
@@ -2,10 +2,10 @@ import { useEffect, useState } from 'react';
 
 import { DateTime } from 'luxon';
 
-import { FudIcon, RealmGif } from 'components/Icons/Icons';
+import { ChannelIcon } from 'components/Icons/Icons';
 import CustomTooltip from 'components/custom/CustomTooltip';
 import realmApi from 'api/realm.api';
-import { DAY_MILLIS } from 'data/date';
+import { DAY_MILLIS, HOUR_MILLIS, MINUTE_MILLIS, SECOND_MILLIS } from 'data/date';
 
 import styles from './styles';
 import ContentLoader from 'react-content-loader';
@@ -44,6 +44,26 @@ export default function GotchiChanelling({ gotchiId }) {
         return dateDiff > DAY_MILLIS;
     };
 
+    const getUTCDayMilis = (timestamp) => {
+        const utc = DateTime.fromMillis(timestamp).setZone('UTC');
+        const hours = utc.hour * HOUR_MILLIS;
+        const minutes = utc.minute * MINUTE_MILLIS;
+        const seconds = utc.second * SECOND_MILLIS;
+
+        return hours + minutes + seconds;
+    };
+
+    const chanelledBeforeCd = (date) => {
+        if (moreThan24hours(date)) {
+            return true;
+        }
+
+        const last = getUTCDayMilis(date);
+        const now = getUTCDayMilis(DateTime.local().toMillis());
+
+        return last - now > 0;
+    };
+
     return (
         <div className={classes.container}>
             { lastChannelingLoading ? (
@@ -69,15 +89,10 @@ export default function GotchiChanelling({ gotchiId }) {
                     followCursor
                 >
                     <div>
-                        { !atLeastOneTimeChanneled(lastChanneling) ? (
-                            <span style={{ color: 'orange', fontSize: 18, fontWeight: 'bold' }}>N</span>
-
+                        { chanelledBeforeCd(lastChanneling) ? (
+                            <ChannelIcon height={20} width={20} />
                         ) : (
-                            moreThan24hours(lastChanneling) ? (
-                                <RealmGif height={24} width={24} />
-                            ) : (
-                                <FudIcon className={classes.unactiveIcon} height={24} width={28} />
-                            )
+                            <ChannelIcon className={classes.unactiveIcon} height={20} width={20} />
                         )}
                     </div>
                 </CustomTooltip>
