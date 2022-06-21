f05a7800a65e3ee1253e73594ec2d68dd2adfa53
Dmytro Kot <dmutrokot94@gmail.com>
2022-06-21 16:08:13 +0300

    tech: Fix lint

--- src/hooks/useFullscreenStatus.ts
+++ src/hooks/useFullscreenStatus.ts
@@ -49,9 +49,9 @@ function getBrowserFullscreenElementProp() {
         return 'fullscreenElement';
     } else if (typeof document['mozFullScreenElement'] !== 'undefined') {
         return 'mozFullScreenElement';
-    } else if (typeof document['mozFullScreenElement'] !== 'undefined') {
+    } else if (typeof document['msFullscreenElement'] !== 'undefined') {
         return 'msFullscreenElement';
-    } else if (typeof document['mozFullScreenElement'] !== 'undefined') {
+    } else if (typeof document['webkitFullscreenElement'] !== 'undefined') {
         return 'webkitFullscreenElement';
     } else {
         return null;
