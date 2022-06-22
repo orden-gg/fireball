08e1827766bad046f64e8b19e70d19cb6ee66471
Dmytro Kot <dmutrokot94@gmail.com>
2022-06-22 22:45:25 +0300

    tech: Fix tickets styles on Tickets

--- src/components/Items/Ticket/TicketImage.tsx
+++ src/components/Items/Ticket/TicketImage.tsx
@@ -1,11 +1,16 @@
 import { ItemUtils } from 'utils';
 
+import { ERC1155InnerStyles } from '../styles';
+
 export function TicketImage({ ticket }: { ticket: any }) {
+    const classes = ERC1155InnerStyles();
+
     return (
-        <div>
+        <div className={classes.iconWrapper}>
             <img
                 src={ItemUtils.getTicketImg(ticket.name || ItemUtils.getItemRarityName(ticket.erc1155TypeId))}
                 alt={ticket.name || ItemUtils.getItemRarityName(ticket.erc1155TypeId)}
+                className={classes.icon}
             />
         </div>
     );
