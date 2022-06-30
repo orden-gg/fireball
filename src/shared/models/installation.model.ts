68d1f2613334f418d9f4eb187f3edc162f699317
Vladyslav <v2rockua@gmail.com>
2022-06-30 19:16:24 +0200

    fix warnings

--- src/shared/models/installation.model.ts
+++ src/shared/models/installation.model.ts
@@ -1,11 +1,11 @@
 export interface InstallationItem {
-    name: string,
-    type: string,
-    level: number,
-    spillRadius: number,
-    spillRate: number,
-    craftTime: number,
-    alchemicaCost: number[],
-    cooldown: number,
-    deprecated: boolean
-};
+    name: string;
+    type: string;
+    level: number;
+    spillRadius: number;
+    spillRate: number;
+    craftTime: number;
+    alchemicaCost: number[];
+    cooldown: number;
+    deprecated: boolean;
+}
