2e76e26a5a8a249b68b195548d1b57d1889fd266
Vladyslav <v2rockua@gmail.com>
2022-07-15 19:14:26 +0200

    feat: change gotchi id

--- src/shared/constants/homepage.constants.ts
+++ src/shared/constants/homepage.constants.ts
@@ -1,4 +1,4 @@
-export const GOTCHI_IDS: number[] = [4285, 8005, 4282, 23470, 13998, 5127, 3672, 7993, 17759];
+export const GOTCHI_IDS: number[] = [4751, 8005, 4282, 23470, 13998, 5127, 3672, 7993, 17759];
 export const LAST_GOTCHI_SCALE: number = .5;
 export const START_ANGLE: number =  Math.PI / (GOTCHI_IDS.length + 1);
 export const H_D: number = 100;
