bfe6cad0972b2129af5ba08d5c76dc6340e7db23
Vladyslav <v2rockua@gmail.com>
2022-07-16 13:19:58 +0200

    feat: add types

--- src/shared/constants/homepage.constants.ts
+++ src/shared/constants/homepage.constants.ts
@@ -4,7 +4,7 @@ export const START_ANGLE: number =  Math.PI / (GOTCHI_IDS.length + 1);
 export const H_D: number = 100;
 export const V_D: number = 60;
 
-export const MAX_GOTCHIS_IN_ROW = [11, 12, 9];
-export const MAX_ROWS = 3;
-export const EASTER_EGG_VIEW_CHANCE = 3;
+export const MAX_GOTCHIS_IN_ROW: number[] = [11, 12, 9];
+export const MAX_ROWS: number = 3;
+export const EASTER_EGG_VIEW_CHANCE: number = 3;
 
