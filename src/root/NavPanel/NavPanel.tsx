1e252e34493abce06bcd9a8fdbe8988483711527
dudendy <a.dude.koval@gmail.com>
2022-11-27 23:10:57 +0700

    feat: add Anvil link to NavPanel

--- src/root/NavPanel/NavPanel.tsx
+++ src/root/NavPanel/NavPanel.tsx
@@ -4,6 +4,7 @@ import StoreIcon from '@mui/icons-material/Store';
 import ConfirmationNumberIcon from '@mui/icons-material/ConfirmationNumber';
 import AutorenewIcon from '@mui/icons-material/Autorenew';
 import PersonIcon from '@mui/icons-material/Person';
+import BuildCircleIcon from '@mui/icons-material/BuildCircle';
 import MapIcon from '@mui/icons-material/Map';
 import TravelExploreIcon from '@mui/icons-material/TravelExplore';
 
@@ -23,89 +24,97 @@ export function NavPanel() {
     const clientLink = activeAddress ? `/client/${activeAddress}/gotchis` : 'client';
 
     return (
-            <div className={classes.container}>
-                <nav className={classes.navigation}>
-                    <div className={classes.navItem}>
-                        <NavLink className={classes.navLink} to={clientLink}>
-                            <Icon className={classes.iconBox}>
-                                <PersonIcon width={20} height={20} />
-                            </Icon>
-                            <span className={classes.navItemName}>Client</span>
-                        </NavLink>
-                    </div>
-                    <div className={classes.navItem}>
-                        <NavLink className={classes.navLink} to='/lend'>
-                            <Icon className={classes.iconBox}>
-                                <LendingIcon width={22} height={22} />
-                            </Icon>
-                            <span className={classes.navItemName}>Lend</span>
-                        </NavLink>
-                    </div>
-                    <div className={classes.navItem}>
-                        <NavLink className={classes.navLink} to='/market'>
-                            <Icon className={classes.iconBox}>
-                                <StoreIcon width={20} height={20} />
-                            </Icon>
-                            <span className={classes.navItemName}>Market</span>
-                        </NavLink>
-                    </div>
-                    <div className={classes.navItem}>
-                        <NavLink className={classes.navLink} to='/autopet'>
-                            <Icon className={classes.iconBox}>
-                                <AutorenewIcon width={20} height={20} />
-                            </Icon>
-                            <span className={classes.navItemName}>Autopet</span>
-                        </NavLink>
-                    </div>
-                    <div className={classes.navItem}>
-                        <NavLink className={classes.navLink} to='/guilds'>
-                            <Icon className={classes.iconBox}>
-                                <GuildIcon width={24} height={24} />
-                            </Icon>
-                            <span className={classes.navItemName}>Guilds</span>
-                        </NavLink>
-                    </div>
-                    <div className={classes.navItem}>
-                        <NavLink className={classes.navLink} to='/raffles'>
-                            <Icon className={classes.iconBox}>
-                                <ConfirmationNumberIcon width={20} height={20} />
-                            </Icon>
-                            <span className={classes.navItemName}>Raffles</span>
-                        </NavLink>
-                    </div>
-                    <div className={classes.navItem}>
-                        <NavLink className={classes.navLink} to='/map'>
-                            <Icon className={classes.iconBox}>
-                                <MapIcon width={20} height={20} />
-                            </Icon>
-                            <span className={classes.navItemName}>Map</span>
-                        </NavLink>
-                    </div>
-                    <div className={classes.navItem}>
-                        <NavLink className={classes.navLink} to='/craft'>
-                            <Icon className={classes.iconBox}>
-                                <CraftIcon width={24} height={24} />
-                            </Icon>
-                            <span className={classes.navItemName}>Craft</span>
-                        </NavLink>
-                    </div>
-                    <div className={classes.navItem}>
-                        <NavLink className={classNames(classes.navLink)} to='/explorer'>
-                            <Icon className={classes.iconBox}>
-                                <TravelExploreIcon width={20} height={20} />
-                            </Icon>
-                            <span className={classes.navItemName}>Aavegotchi explorer</span>
-                        </NavLink>
-                    </div>
-                    <div className={classes.navItem}>
-                        <NavLink className={classes.navLink} to='/glossary'>
-                            <Icon className={classes.iconBox}>
-                                <GlossaryIcon width={20} height={20} />
-                            </Icon>
-                            <span className={classes.navItemName}>Glossary</span>
-                        </NavLink>
-                    </div>
-                </nav>
-            </div>
+        <div className={classes.container}>
+            <nav className={classes.navigation}>
+                <div className={classes.navItem}>
+                    <NavLink className={classes.navLink} to={clientLink}>
+                        <Icon className={classes.iconBox}>
+                            <PersonIcon width={20} height={20} />
+                        </Icon>
+                        <span className={classes.navItemName}>Client</span>
+                    </NavLink>
+                </div>
+                <div className={classes.navItem}>
+                    <NavLink className={classes.navLink} to='/lend'>
+                        <Icon className={classes.iconBox}>
+                            <LendingIcon width={22} height={22} />
+                        </Icon>
+                        <span className={classes.navItemName}>Lend</span>
+                    </NavLink>
+                </div>
+                <div className={classes.navItem}>
+                    <NavLink className={classes.navLink} to='/market'>
+                        <Icon className={classes.iconBox}>
+                            <StoreIcon width={20} height={20} />
+                        </Icon>
+                        <span className={classes.navItemName}>Market</span>
+                    </NavLink>
+                </div>
+                <div className={classes.navItem}>
+                    <NavLink className={classes.navLink} to='/autopet'>
+                        <Icon className={classes.iconBox}>
+                            <AutorenewIcon width={20} height={20} />
+                        </Icon>
+                        <span className={classes.navItemName}>Autopet</span>
+                    </NavLink>
+                </div>
+                <div className={classes.navItem}>
+                    <NavLink className={classes.navLink} to='/guilds'>
+                        <Icon className={classes.iconBox}>
+                            <GuildIcon width={24} height={24} />
+                        </Icon>
+                        <span className={classes.navItemName}>Guilds</span>
+                    </NavLink>
+                </div>
+                <div className={classes.navItem}>
+                    <NavLink className={classes.navLink} to='/raffles'>
+                        <Icon className={classes.iconBox}>
+                            <ConfirmationNumberIcon width={20} height={20} />
+                        </Icon>
+                        <span className={classes.navItemName}>Raffles</span>
+                    </NavLink>
+                </div>
+                <div className={classes.navItem}>
+                    <NavLink className={classes.navLink} to='/map'>
+                        <Icon className={classes.iconBox}>
+                            <MapIcon width={20} height={20} />
+                        </Icon>
+                        <span className={classes.navItemName}>Map</span>
+                    </NavLink>
+                </div>
+                <div className={classes.navItem}>
+                    <NavLink className={classes.navLink} to='/craft'>
+                        <Icon className={classes.iconBox}>
+                            <CraftIcon width={24} height={24} />
+                        </Icon>
+                        <span className={classes.navItemName}>Craft</span>
+                    </NavLink>
+                </div>
+                <div className={classes.navItem}>
+                    <NavLink className={classes.navLink} to='/anvil'>
+                        <Icon className={classes.iconBox}>
+                            <BuildCircleIcon width={20} height={20} />
+                        </Icon>
+                        <span className={classes.navItemName}>Anvil</span>
+                    </NavLink>
+                </div>
+                <div className={classes.navItem}>
+                    <NavLink className={classNames(classes.navLink)} to='/explorer'>
+                        <Icon className={classes.iconBox}>
+                            <TravelExploreIcon width={20} height={20} />
+                        </Icon>
+                        <span className={classes.navItemName}>Aavegotchi explorer</span>
+                    </NavLink>
+                </div>
+                <div className={classes.navItem}>
+                    <NavLink className={classes.navLink} to='/glossary'>
+                        <Icon className={classes.iconBox}>
+                            <GlossaryIcon width={20} height={20} />
+                        </Icon>
+                        <span className={classes.navItemName}>Glossary</span>
+                    </NavLink>
+                </div>
+            </nav>
+        </div>
     );
 }
