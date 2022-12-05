45552f826eb485fa345d543154cf8a5e3341af8e
dudendy <a.dude.koval@gmail.com>
2022-12-05 08:27:13 +0700

    feat: update team gotchi ids

--- src/shared/constants/homepage.constants.ts
+++ src/shared/constants/homepage.constants.ts
@@ -1,10 +1,9 @@
-export const GOTCHI_IDS: number[] = [4751, 7788, 4282, 23470, 13998, 5127, 3672, 7993, 17759];
+export const GOTCHI_IDS: number[] = [4751, 7788, 23470, 5127, 3672, 7993, 17759];
 export const LAST_GOTCHI_SCALE: number = 0.5;
-export const START_ANGLE: number =  Math.PI / (GOTCHI_IDS.length + 1);
+export const START_ANGLE: number = Math.PI / (GOTCHI_IDS.length + 1);
 export const H_D: number = 100;
 export const V_D: number = 60;
 
 export const MAX_GOTCHIS_IN_ROW: number[] = [11, 12, 9];
 export const MAX_ROWS: number = 3;
 export const EASTER_EGG_VIEW_CHANCE: number = 3;
-
