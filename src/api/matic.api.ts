5ba0d6d7a3d143061e1ea184229d173b181b2fe4
Stx69 <rudisek@seznam.cz>
2023-02-20 16:23:09 +0100

    Any any out from matic.api.ts

--- src/api/matic.api.ts
+++ src/api/matic.api.ts
@@ -2,11 +2,12 @@ import { EthersApi } from './ethers.api';
 
 import { MATIC_CONTRACT } from 'shared/constants';
 import { MATIC_ABI } from 'data/abi/matic.abi';
+import { BigNumber } from 'ethers';
 
 const contract = EthersApi.makeContract(MATIC_CONTRACT, MATIC_ABI, 'polygon');
 
 export class MaticApi {
-  public static getBalanceOf(address: any): string {
-    return contract.balanceOf(address).then((balance: any) => EthersApi.hexToNumber(balance._hex));
+  public static getBalanceOf(address: string): string {
+    return contract.balanceOf(address).then((balance: BigNumber) => EthersApi.hexToNumber(balance._hex));
   }
 }
