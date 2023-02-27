2005a994b09d91c0c314638738872ff2826d77dc
Vladyslav <v2rockua@gmail.com>
2023-02-27 12:27:30 +0700

    tech: delete graphs

--- subgraph/gotchiverse/src/helpers/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './gotchi.helper';
-export * from './installation.helper';
-export * from './parcel.helper';
-export * from './player.helper';
-export * from './survey.helper';
-export * from './tile.helper';
-export * from './ERC721Listings.helper';
