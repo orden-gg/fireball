6b867736370ae071a132931cfa70d56b0152e225
Stx69 <rudisek@seznam.cz>
2023-03-06 16:15:14 +0100

    Add "Id" to items in glossary

--- src/pages/Glossary/components/GlossaryWearables/GlossaryWearables.tsx
+++ src/pages/Glossary/components/GlossaryWearables/GlossaryWearables.tsx
@@ -227,6 +227,7 @@ export function GlossaryWearables() {
               <ItemCard id={wearable.id} category={wearable.category} type={wearable.rarity}>
                 <CardGroup name='header' className={classes.wearableHeader}>
                   <CardSlot id={wearable.id} className={classes.overridedSlot} />
+                  <span className={classes.itemTypeValue}>{wearable.id}</span>
                   <CardBalance balance={`${wearable.totalQuantity}`} holders={[]} />
                 </CardGroup>
                 <CardGroup name='body'>
