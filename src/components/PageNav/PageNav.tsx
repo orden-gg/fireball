33d2e1bcc454e78a8221c9f6da76a29151fea47e
ainkhm <90315286+NatashaTGA@users.noreply.github.com>
2023-03-14 10:26:59 +0200

    create renderNavButton method for PageNav(render Button)

--- src/components/PageNav/PageNav.tsx
+++ src/components/PageNav/PageNav.tsx
@@ -26,47 +26,53 @@ export function PageNav({ links, beforeContent, afterContent }: PageNavProps) {
 
   const data = useMemo(() => links, [links]);
 
+  function renderNavButton(link, index) {
+    return (
+      <Button
+        disabled={link.count === 0}
+        startIcon={link.icon}
+        component={NavLink}
+        className={classNames(classes.button, link.count === undefined && classes.onlyIconBtn)}
+        to={link.path}
+        key={index}
+      >
+        {link.name && <span className={classes.navName}>{link.name}</span>}
+        {link.count !== undefined ? (
+          <>
+            {link.isLoading ? (
+              <ContentLoader
+                speed={2}
+                viewBox='0 0 28 14'
+                backgroundColor={theme.palette.secondary.main}
+                foregroundColor={theme.palette.primary.dark}
+                className={classes.buttonLoader}
+              >
+                <rect x='0' y='0' width='28' height='14' />
+              </ContentLoader>
+            ) : (
+              <span className={classes.label}>[{link.count}]</span>
+            )}
+          </>
+        ) : (
+          <></>
+        )}
+      </Button>
+    );
+  }
+
   return (
     <div className={classes.container}>
       {beforeContent}
       {data.map((link: PageNavLink, index: number) => {
-        return (
+        return link.isShowSubRoutes ? (
           <div className={classes.navItem} key={index}>
-            <CustomPopup
-              title={
-                <Button
-                  disabled={link.count === 0}
-                  startIcon={link.icon}
-                  component={NavLink}
-                  className={classNames(classes.button, link.count === undefined && classes.onlyIconBtn)}
-                  to={link.path}
-                  key={index}
-                >
-                  {link.name && <span className={classes.navName}>{link.name}</span>}
-                  {link.count !== undefined ? (
-                    <>
-                      {link.isLoading ? (
-                        <ContentLoader
-                          speed={2}
-                          viewBox='0 0 28 14'
-                          backgroundColor={theme.palette.secondary.main}
-                          foregroundColor={theme.palette.primary.dark}
-                          className={classes.buttonLoader}
-                        >
-                          <rect x='0' y='0' width='28' height='14' />
-                        </ContentLoader>
-                      ) : (
-                        <span className={classes.label}>[{link.count}]</span>
-                      )}
-                    </>
-                  ) : (
-                    <></>
-                  )}
-                </Button>
-              }
-            >
-              <>{link.isShowSubRoutes && <div className={classes.subNavWrapper}>{link.subNavComponent}</div>}</>
+            <CustomPopup title={renderNavButton(link, index)}>
+              <div className={classes.subNavWrapper}>{link.subNavComponent}</div>
             </CustomPopup>
+          </div>
+        ) : (
+          <div className={classes.navItem} key={index}>
+            {renderNavButton(link, index)}
           </div>
         );
       })}
