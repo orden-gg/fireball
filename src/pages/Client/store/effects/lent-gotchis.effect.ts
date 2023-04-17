f207890e2e05252a0b28d581f8ddd4143c3ab0c7
Stx69 <rudisek@seznam.cz>
2023-04-17 21:35:10 +0200

    bugfix - lent gotchis effects

--- src/pages/Client/store/effects/lent-gotchis.effect.ts
+++ src/pages/Client/store/effects/lent-gotchis.effect.ts
@@ -2,7 +2,14 @@ import { TheGraphApi } from 'api';
 
 import { AppThunk } from 'core/store/store';
 
-import { FireballGotchi, GotchiLending, GotchiLendingExtended, SortingItem, TheGraphBatchData } from 'shared/models';
+import {
+  FireballGotchi,
+  GotchiLastChanneled,
+  GotchiLending,
+  GotchiLendingExtended,
+  SortingItem,
+  TheGraphBatchData
+} from 'shared/models';
 
 import { CommonUtils } from 'utils';
 
@@ -16,38 +23,51 @@ export const onLoadLentGotchis =
 
     const { type, dir }: SortingItem = getState().client.lentGotchis.lentGotchisSorting;
 
-    TheGraphApi.getLendingsByAddress(address)
-      .then((lentGotchis: GotchiLending[]) => {
-        lentGotchis.forEach((lending: GotchiLending) => {
-          lending.endTime = Number(lending.timeAgreed) + Number(lending.period);
-        });
-
-        const sortedLentGotchis: GotchiLending[] = CommonUtils.basicSort(lentGotchis, type, dir);
-        const gotchiIds: number[] = sortedLentGotchis.map((gotchi: GotchiLending) => Number(gotchi.id));
-
-        if (gotchiIds.length > 0) {
-          TheGraphApi.getFireballGotchisByIds(gotchiIds)
-            .then((fireballGotchis: TheGraphBatchData<FireballGotchi>) => {
-              const extendedLendingGotchis: GotchiLendingExtended[] = sortedLentGotchis.map(
-                (lending: GotchiLending) => {
-                  return { ...lending, ...fireballGotchis[`gotchi${lending.id}`] };
-                }
-              );
-
-              dispatch(lentGotchisSlices.loadLentGotchisSucceded(extendedLendingGotchis));
-            })
-            .catch(() => {
-              dispatch(lentGotchisSlices.loadLentGotchisFailed());
-              dispatch(lentGotchisSlices.loadLentGotchisSucceded(sortedLentGotchis));
-            })
-            .finally(() => dispatch(lentGotchisSlices.setIsInitialLentGotchisLoading(false)));
-        } else {
-          dispatch(lentGotchisSlices.loadLentGotchisSucceded(sortedLentGotchis));
-          dispatch(lentGotchisSlices.setIsInitialLentGotchisLoading(false));
-        }
-      })
-      .catch(() => {
-        dispatch(lentGotchisSlices.loadLentGotchisFailed());
-        dispatch(lentGotchisSlices.setIsInitialLentGotchisLoading(false));
+    TheGraphApi.getLendingsByAddress(address).then((lentGotchis: GotchiLending[]) => {
+      lentGotchis.forEach((lending: GotchiLending) => {
+        lending.endTime = Number(lending.timeAgreed) + Number(lending.period);
       });
+
+      const gotchiIds: string[] = lentGotchis.map((gotchi) => gotchi.id);
+      TheGraphApi.getGotchisGotchiverseInfoByIds(gotchiIds)
+        .then((gotchiIdsChanneled: GotchiLastChanneled[]) => {
+          const modifiedLent: GotchiLending[] = lentGotchis.map((item: GotchiLending) => {
+            const lastChanneledAlchemica = gotchiIdsChanneled.find((o: GotchiLastChanneled) => o.id === item.id);
+
+            return {
+              ...item,
+              lastChanneledAlchemica: lastChanneledAlchemica?.lastChanneledAlchemica
+                ? lastChanneledAlchemica?.lastChanneledAlchemica
+                : '0'
+            };
+          });
+          const sortedLentGotchis: GotchiLending[] = CommonUtils.basicSort(modifiedLent, type, dir);
+          const gotchiIds: number[] = sortedLentGotchis.map((gotchi: GotchiLending) => Number(gotchi.id));
+
+          if (gotchiIds.length > 0) {
+            TheGraphApi.getFireballGotchisByIds(gotchiIds)
+              .then((fireballGotchis: TheGraphBatchData<FireballGotchi>) => {
+                const extendedLendingGotchis: GotchiLendingExtended[] = sortedLentGotchis.map(
+                  (lending: GotchiLending) => {
+                    return { ...lending, ...fireballGotchis[`gotchi${lending.id}`] };
+                  }
+                );
+
+                dispatch(lentGotchisSlices.loadLentGotchisSucceded(extendedLendingGotchis));
+              })
+              .catch(() => {
+                dispatch(lentGotchisSlices.loadLentGotchisFailed());
+                dispatch(lentGotchisSlices.loadLentGotchisSucceded(sortedLentGotchis));
+              })
+              .finally(() => dispatch(lentGotchisSlices.setIsInitialLentGotchisLoading(false)));
+          } else {
+            dispatch(lentGotchisSlices.loadLentGotchisSucceded(sortedLentGotchis));
+            dispatch(lentGotchisSlices.setIsInitialLentGotchisLoading(false));
+          }
+        })
+        .catch(() => {
+          dispatch(lentGotchisSlices.loadLentGotchisFailed());
+          dispatch(lentGotchisSlices.setIsInitialLentGotchisLoading(false));
+        });
+    });
   };
