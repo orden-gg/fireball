8770d23a97931d5f5546bcb1c8e07d5103fdccca
airmangataos <dmutrokot94@gmail.com>
2023-05-26 15:28:23 +0300

    #581 feat: Add mainnet diamond address and subgraph

--- src/pages/Guilds/constants/guild.api.constants.ts
+++ src/pages/Guilds/constants/guild.api.constants.ts
@@ -1,5 +1,5 @@
-export const GUIlD_GRAPH_API = 'http://localhost:8000/subgraphs/name/guilds';
-export const GUILD_CONTRACT = '0xc3e53F4d16Ae77Db1c982e75a937B9f60FE63690';
+export const GUIlD_GRAPH_API = 'https://api.thegraph.com/subgraphs/name/orden-gg/guilds';
+export const GUILD_CONTRACT = '0xD67A94D71691532F5EBB037f772D9AC4b285288B';
 
 export const GUIlD_GRAPH_STATS_API = 'https://api.thegraph.com/subgraphs/name/dudendy/kratoman';
 export const GUIlD_GRAPH_REALM_STATS_API = 'https://api.thegraph.com/subgraphs/name/dudendy/gotchiverse';
