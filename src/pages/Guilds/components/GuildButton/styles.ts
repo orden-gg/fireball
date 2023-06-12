af15e8de6e648287e66e9341cd31e11cb09b9101
Vladyslav <v2rockua@gmail.com>
2023-06-12 17:05:04 +0700

    refactor: rework guild button

---
+++ src/pages/Guilds/components/GuildButton/styles.ts
@@ -0,0 +1,15 @@
+import { Button, alpha, lighten } from '@mui/material';
+import { styled } from '@mui/styles';
+
+export const ButtonStyled = styled(Button)(({ theme }) => ({
+  color: alpha(theme.palette.common.white, 0.5),
+  borderColor: lighten(theme.palette.background.default, 0.25),
+  paddingLeft: theme.spacing(3),
+  paddingRight: theme.spacing(3),
+  display: 'flex',
+  alignItems: 'center',
+  '&:hover': {
+    borderColor: lighten(theme.palette.background.default, 0.25),
+    backgroundColor: alpha(theme.palette.background.default, 0.35)
+  }
+}));
--- src/pages/Guilds/components/index.ts
+++ src/pages/Guilds/components/index.ts
@@ -1,3 +1,4 @@
 export { GuildLogo } from './GuildLogo/GuildLogo';
 export { GuildWearables } from './GuildWearables/GuildWearables';
 export { GuildSocials } from './GuildSocials/GuildSocials';
+export { GuildButton } from './GuildButton/GuildButton';
