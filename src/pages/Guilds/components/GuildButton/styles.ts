d1573224935df8b3bee75a0f66ac5460db55bcb5
Vladyslav <v2rockua@gmail.com>
2023-06-13 13:31:37 +0700

    style: highlite button text color

--- src/pages/Guilds/components/GuildButton/styles.ts
+++ src/pages/Guilds/components/GuildButton/styles.ts
@@ -2,7 +2,7 @@ import { Button, alpha, lighten } from '@mui/material';
 import { styled } from '@mui/styles';
 
 export const ButtonStyled = styled(Button)(({ theme }) => ({
-  color: alpha(theme.palette.common.white, 0.5),
+  color: alpha(theme.palette.common.white, 0.8),
   borderColor: lighten(theme.palette.background.default, 0.25),
   paddingLeft: theme.spacing(3),
   paddingRight: theme.spacing(3),
