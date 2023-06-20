88a61895f72fa37d69556c02809367d24ee78528
dudendy <a.dude.koval@gmail.com>
2023-06-20 11:14:03 +0300

    feat: managing scripts (#590)
    * feat(scripts): v1 of claim, agree and lend scripts
    
    * fix: lint errors
    
    * feat: claim script v2
    
    * fix: lint errors
    
    * feat: lend v2 - batch balance, txs
    
    * feat: push scripts data
    
    * fix: lint errors
    
    * feat: claim script batch owner addresses
    
    * fix: lint issue
    
    * feat: callWithRetries for claim script
    
    * feat: add pet-batch
    
    * fix: lint erors
    
    * feat: add scripts README
    
    * fix: lint errors
    
    * fix: scripts CustomAny
    
    * feat: add scripts.toml, update scripts according to the new structure
    
    * fix: README.md
    
    * fix: README.md again
    
    * fix: scripts.toml
    
    * feat: upgrade claim, lend scripts
    
    * feat: log ids at lending script
    
    * feat: update main readme

---
+++ src/data/scripts/agree.script.ts
@@ -0,0 +1,123 @@
+import axios from 'axios';
+import 'dotenv/config';
+import { BigNumber, ContractTransaction, ethers } from 'ethers';
+import { exit } from 'process';
+
+// @ts-ignore
+import {
+  CONSOLE_COLORS,
+  MAIN_CONTRACT_WITH_SIGNER,
+  SCRIPT_WALLET_ADDRESS,
+  SCRIPT_WALLET_NONCE,
+  SETTINGS,
+  TX_COST_LIMIT,
+  getGasPrice,
+  paint // @ts-ignore
+} from './api/scripts.api.ts';
+
+// @ts-ignore
+import { GRAPH_CORE_API } from '../../shared/constants/the-graph.constants.ts';
+
+const { OPERATOR_PRIVATE_KEY } = process.env;
+
+// TODO: batch claim
+const lendingsQuery = `{
+  gotchiLendings(
+    first: 10,
+    orderBy: gotchiKinship,
+    orderDir: desc,
+    where:{
+      whitelistId: ${SETTINGS.WHITELIST}
+      cancelled: false,
+      completed: false,
+      timeAgreed: null
+    }
+  ) {
+    id
+    gotchiTokenId
+    gotchiKinship
+    whitelistId
+    upfrontCost
+    period
+    splitOwner
+    splitBorrower
+    splitOther
+  }
+}`;
+
+console.log(`🧑 operator: ${paint(SCRIPT_WALLET_ADDRESS, CONSOLE_COLORS.Pink)}`);
+
+const agree = async () => {
+  if (!OPERATOR_PRIVATE_KEY) {
+    console.log('Please specify OPERTOR_PRIVATE_KEY in .env');
+    exit();
+  }
+
+  return await axios.post(GRAPH_CORE_API, { query: lendingsQuery }).then(async (response) => {
+    if (response.data.errors) {
+      console.log(paint('error!', CONSOLE_COLORS.Red), response.data.errors);
+    }
+
+    const lendings = response.data.data.gotchiLendings;
+
+    console.log('agree', lendings.length, 'lendings');
+
+    const gasPriceGwei = await getGasPrice();
+    const gasPrice = ethers.utils.formatUnits(gasPriceGwei, 'gwei');
+    const gasBoost = ethers.utils.parseUnits('25', 'gwei');
+    const gasBoosted = BigNumber.from(gasPriceGwei).add(gasBoost);
+
+    const nonce = await SCRIPT_WALLET_NONCE();
+
+    if (gasPriceGwei >= TX_COST_LIMIT) {
+      console.log(
+        `💱 ${paint('to high tx cost: maximum', CONSOLE_COLORS.Red)} ${paint(
+          TX_COST_LIMIT.toString(),
+          CONSOLE_COLORS.Red
+        )} current ${paint(gasPriceGwei, CONSOLE_COLORS.Pink)}`
+      );
+      console.log(paint('Terminated!', CONSOLE_COLORS.Red));
+
+      return;
+    }
+
+    console.log(`💱 tx cost: maximum - ${TX_COST_LIMIT} current - ${paint(gasPriceGwei, CONSOLE_COLORS.Pink)}`);
+    console.log(`🚀 gas price: ${paint(Number(gasPrice).toFixed(2), CONSOLE_COLORS.Pink)}`);
+    console.log(`🗞  current nonce: ${paint(nonce, CONSOLE_COLORS.Pink)}`);
+
+    const promises = lendings.map((lending, index) =>
+      MAIN_CONTRACT_WITH_SIGNER.agreeGotchiLending(
+        lending.id,
+        lending.gotchiTokenId,
+        lending.upfrontCost,
+        lending.period,
+        [lending.splitOwner, lending.splitBorrower, lending.splitOther],
+        { gasPrice: gasBoosted, gasLimit: 2000000, nonce: nonce + index }
+      )
+    );
+
+    Promise.all(promises).then(async (txsRes: ContractTransaction[]) => {
+      for (const [index, tx] of txsRes.entries()) {
+        console.log(`${paint('Tx sent!', CONSOLE_COLORS.Green)} https://polygonscan.com/tx/${tx.hash}`);
+
+        tx.wait()
+          .then(() =>
+            console.log(
+              paint('=>', CONSOLE_COLORS.Green),
+              paint(lendings[index].gotchiTokenId, CONSOLE_COLORS.Pink),
+              paint('transaction confirmed!', CONSOLE_COLORS.Green)
+            )
+          )
+          .catch(() =>
+            console.log(
+              paint('=>', CONSOLE_COLORS.Red),
+              paint(lendings[index].gotchiTokenId, CONSOLE_COLORS.Pink),
+              paint('transaction failed :(', CONSOLE_COLORS.Red)
+            )
+          );
+      }
+    });
+  });
+};
+
+agree();
--- src/data/scripts/tiles.script.ts
+++ src/data/scripts/tiles.script.ts
@@ -1,28 +1,18 @@
-import 'dotenv/config';
 import ethers from 'ethers';
 import fs from 'fs';
 import _ from 'lodash';
-
-// @ts-ignore
-import { TILES_CONTRACT } from '../../shared/constants/api.constants.ts';
-
-// @ts-ignore
-import { TileTypes } from '../../shared/constants/enums/enums.ts';
 // @ts-ignore
 import { CustomAny } from 'types/global.js';
 
-const abiFile = fs.readFileSync('src/data/abi/tiles.abi.json');
-const TILES_ABI = JSON.parse(abiFile.toString());
+// @ts-ignore
+import { TILES_CONTRACT_WITH_SIGNER } from './api/scripts.api.ts';
 
-const provider = new ethers.providers.JsonRpcProvider(
-  `https://polygon-mainnet.infura.io/v3/${process.env.POLYGON_PROVIDER_API_KEY}`
-);
-const tilesContract = new ethers.Contract(TILES_CONTRACT, TILES_ABI, provider);
+// @ts-ignore
+import { TileTypes } from '../../shared/constants/enums/enums.ts';
 
 console.log('⏳ retrieving data from blockhain ⌛');
 
-tilesContract
-  .getTileTypes([])
+TILES_CONTRACT_WITH_SIGNER.getTileTypes([])
   .then((res: CustomAny) => {
     const modified = _.cloneDeep(res);
 
