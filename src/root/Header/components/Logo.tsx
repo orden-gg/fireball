bddcbeef81cdffc24d67219926c94cfb7305538a
dudendy <a.dude.koval@gmail.com>
2023-06-20 16:06:37 +0700

    feat: add Pavel credentials

--- src/root/Header/components/Logo.tsx
+++ src/root/Header/components/Logo.tsx
@@ -11,6 +11,24 @@ export function Logo() {
     <NavLink className={classes.logoWrapper} to='/'>
       <LogoPilotIcon className={classes.logoDesktop} width={80} height={34} />
       <MobileLogoIcon width={24} height={36} className={classes.logoMobile} />
+      <div
+        style={{
+          position: 'absolute',
+          bottom: 0,
+          left: 0,
+          whiteSpace: 'nowrap',
+          fontSize: 8
+        }}
+      >
+        by{' '}
+        <a
+          href='https://twitter.com/PavelRudolf3'
+          target='_blank'
+          style={{ color: 'black', backgroundColor: 'rgba(255, 255, 255, 0.5)', padding: '0 2px' }}
+        >
+          9STX6
+        </a>
+      </div>
     </NavLink>
   );
 }
