ccac9c51519cb0285140cceeba8feadbdc0a18ce
Stx69 <rudisek@seznam.cz>
2023-07-01 22:47:08 +0200

    feat- add to filters borrowed Client Whitelist & OriginalOwner

--- src/pages/Client/store/effects/borrowed-gotchis.effect.ts
+++ src/pages/Client/store/effects/borrowed-gotchis.effect.ts
@@ -2,7 +2,14 @@ import { TheGraphApi } from 'api';
 
 import { AppThunk } from 'core/store/store';
 
-import { GotchiLastChanneled, FireballGotchi, GotchiLending, GotchiLendingExtended, SortingItem, TheGraphBatchData } from 'shared/models';
+import {
+  FireballGotchi,
+  GotchiLastChanneled,
+  GotchiLending,
+  GotchiLendingExtended,
+  SortingItem,
+  TheGraphBatchData
+} from 'shared/models';
 
 import { CommonUtils } from 'utils';
 
@@ -18,46 +25,48 @@ export const onLoadBorrowedGotchis =
 
     TheGraphApi.getBorrowedByAddress(address).then((borrowedGotchis: GotchiLending[]) => {
       const gotchiIds: string[] = borrowedGotchis.map((gotchi) => gotchi.id);
+
       TheGraphApi.getGotchisGotchiverseInfoByIds(gotchiIds)
         .then((gotchiIdsChanneled: GotchiLastChanneled[]) => {
           const modifiedBorrowed: GotchiLending[] = borrowedGotchis.map((item: GotchiLending) => {
             const lastChanneledAlchemica = gotchiIdsChanneled.find((o: GotchiLastChanneled) => o.id === item.id);
+            const originalOwnerId = item.gotchi.originalOwner.id;
 
             return {
               ...item,
               lastChanneledAlchemica: lastChanneledAlchemica?.lastChanneledAlchemica
                 ? lastChanneledAlchemica?.lastChanneledAlchemica
-                : '0'
+                : '0',
+              originalOwnerId: originalOwnerId ? originalOwnerId : ''
             };
           });
 
           const sortedBorrowedGotchis: GotchiLending[] = CommonUtils.basicSort(modifiedBorrowed, type, dir);
-        const gotchiIds: number[] = sortedBorrowedGotchis.map((gotchi: GotchiLending) => Number(gotchi.id));
+          const gotchiIds: number[] = sortedBorrowedGotchis.map((gotchi: GotchiLending) => Number(gotchi.id));
 
-          
           if (gotchiIds.length > 0) {
-          TheGraphApi.getFireballGotchisByIds(gotchiIds)
-            .then((fireballGotchis: TheGraphBatchData<FireballGotchi>) => {
-              const extendedLendingGotchis: GotchiLendingExtended[] = sortedBorrowedGotchis.map(
-                (lending: GotchiLending) => {
-                  return { ...lending, ...fireballGotchis[`gotchi${lending.id}`] };
-                }
-              );
-              dispatch(borrowedGotchisSlices.loadBorrowedGotchisSucceded(extendedLendingGotchis));
-            })
-            .catch(() => {
-              dispatch(borrowedGotchisSlices.loadBorrowedGotchisFailed());
-              dispatch(borrowedGotchisSlices.loadBorrowedGotchisSucceded(sortedBorrowedGotchis));
+            TheGraphApi.getFireballGotchisByIds(gotchiIds)
+              .then((fireballGotchis: TheGraphBatchData<FireballGotchi>) => {
+                const extendedLendingGotchis: GotchiLendingExtended[] = sortedBorrowedGotchis.map(
+                  (lending: GotchiLending) => {
+                    return { ...lending, ...fireballGotchis[`gotchi${lending.id}`] };
+                  }
+                );
+                dispatch(borrowedGotchisSlices.loadBorrowedGotchisSucceded(extendedLendingGotchis));
+              })
+              .catch(() => {
+                dispatch(borrowedGotchisSlices.loadBorrowedGotchisFailed());
+                dispatch(borrowedGotchisSlices.loadBorrowedGotchisSucceded(sortedBorrowedGotchis));
               })
               .finally(() => dispatch(borrowedGotchisSlices.setIsInitialBorrowedGotchisLoading(false)));
-        } else {
-          dispatch(borrowedGotchisSlices.loadBorrowedGotchisSucceded([]));
+          } else {
+            dispatch(borrowedGotchisSlices.loadBorrowedGotchisSucceded([]));
+            dispatch(borrowedGotchisSlices.setIsInitialBorrowedGotchisLoading(false));
+          }
+        })
+        .catch(() => {
+          dispatch(borrowedGotchisSlices.loadBorrowedGotchisFailed());
           dispatch(borrowedGotchisSlices.setIsInitialBorrowedGotchisLoading(false));
-        }
-      })
-      .catch(() => {
-        dispatch(borrowedGotchisSlices.loadBorrowedGotchisFailed());
-          dispatch(borrowedGotchisSlices.setIsInitialBorrowedGotchisLoading(false));
-      });
+        });
     });
   };
