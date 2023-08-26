ad0ef9abee47660eb7902f7100761349b9f38541
Stx69 <rudisek@seznam.cz>
2023-08-26 18:58:24 +0200

    graph constant adjusted according last dude update

--- src/shared/constants/the-graph.constants.ts
+++ src/shared/constants/the-graph.constants.ts
@@ -1,5 +1,5 @@
 export const GRAPH_CORE_API: string =
-  'https://subgraph.satsuma-prod.com/tWYl5n5y04oz/aavegotchi/aavegotchi-core-matic/api';
+  'https://subgraph.satsuma-prod.com/tWYl5n5y04oz/aavegotchi/aavegotchi-core-matic/version/v2.0.1/api';
 export const GRAPH_RAFFLE_API: string = 'https://api.thegraph.com/subgraphs/name/froid1911/aavegotchi-raffles';
 export const GRAPH_GOTCHI_SVG_API: string = 'https://api.thegraph.com/subgraphs/name/aavegotchi/aavegotchi-svg';
 export const GRAPH_REALM_API: string = 'https://api.thegraph.com/subgraphs/name/aavegotchi/aavegotchi-realm-matic';
